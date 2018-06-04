e30868be03128d1011949c53d1a8a3f9bc4a904a
GuoShuai <msn4399@163.com>
2018-06-03 20:32:33 -0700

    change port number to 57000

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.use(body(option={formLimit: '10mb'}));
 app.use(router.routes());
 app.use(serve('./static'));
 
-app.listen(3000);
-console.log('Koa server started at http://127.0.0.1:3000');
+app.listen(57000);
+console.log('Koa server started at http://127.0.0.1:57000');
