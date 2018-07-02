aa7ad0a28aafa556431fdc812fc59e66669a710f
Guo Shuai <msn4399@163.com>
2018-07-02 19:42:30 +0800

    fix bugs

--- controller.js
+++ controller.js
@@ -1,21 +1,16 @@
 const fs = require('fs');
-const exec = require('child-process-promise').exec;
+const {execSync} = require('child_process');
 
 const asr = async (ctx) => {
     let demo_dir = '../online_demo/';
     let wav_output = '../online_demo/online-data/audio_auto/upload.wav';
     ctx.response.type = 'application/json';
     let file = ctx.request.files[0];
-    let reader = fs.createReadStream(file.path);
-    let stream = fs.createWriteStream('./upload.ogg');
-    reader.pipe(stream);
-    console.log('Uploaded to %s', file.name, stream.path);
 
-    let ffmpeg = await exec(`ffmpeg -y -hide_banner -i upload.ogg -ar 16000 -ac 1 ${wav_output}`);
-    let run = await exec(demo_dir + 'run_auto.sh');
-    
+    execSync(`ffmpeg -y -hide_banner -i ${file.path} -ar 16000 -ac 1 ${wav_output}`);
+    let decodeResult = execSync('bash ' + demo_dir + 'auto_run.sh');
     ctx.response.body = {
-        'msg': run.stdout
+        'msg': decodeResult.toString()
     };
 };
 
